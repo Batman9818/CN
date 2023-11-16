28c9257666a9abf67d01b1906449ae5b9bb0d8c7
Batman9818 <150887683+Batman9818@users.noreply.github.com>
2023-11-16 23:55:16 +0530

    Update exp3_client.c

--- exp3_client.c
+++ exp3_client.c
@@ -1,40 +1,51 @@
-#include<stdio.h> 
-#include<string.h> 
-#include<sys/socket.h> 
-#include<sys/types.h> 
-#include<unistd.h> 
-#include<netinet/in.h> 
-#include<netdb.h> 
-#include<arpa/inet.h> 
-int main(int argc,char * argv[]) 
-{ 
-int cd,sd,ad; 
-char buff[1024]; 
-struct sockaddr_in cliaddr,servaddr; 
-struct hostent *h; 
-/*This function looks up a hostname and it returns a pointer to a hostent 
- structure that contains all the IPV4 address*/ 
-h=gethostbyname(argv[1]); 
-bzero(&servaddr,sizeof(servaddr)); 
-/*Socket address structure*/
-servaddr.sin_family=AF_INET; 
-memcpy((char *)&servaddr.sin_addr.s_addr,h->h_addr_list[0],h->h_length); 
-servaddr.sin_port = htons(1999); 
- /*Creating a socket, assigning IP address and port number for that socket*/ 
-sd = socket(AF_INET,SOCK_STREAM,0); 
- /*Connect establishes connection with the server using server IP address*/ 
-cd=connect(sd,(struct sockaddr*)&servaddr,sizeof(servaddr)); 
-while(1) 
-{ 
-printf("Enter the message: \n"); 
- 
-/*Reads the message from standard input*/ 
- fgets(buff,100,stdin); 
- /*Send function is used on client side to send data given by user on client 
- side to the server*/ 
-send(sd,buff,sizeof(buff)+1,0); 
-printf("\n Data Sent "); 
-//recv(sd,buff,strlen(buff)+1,0); 
-printf("%s",buff); 
-} 
-} 
+#include<sys/socket.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<sys/types.h>
+#define PORT 2345
+#define MAX_BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[MAX_BUFFER_SIZE];
+
+    // Create socket file descriptor
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set up sockaddr_in structure
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        // Send data to the server
+        printf("Client: ");
+        fgets(buffer, MAX_BUFFER_SIZE, stdin);
+        send(sock, buffer, strlen(buffer), 0);
+	bzero(buffer,sizeof(buffer));
+        // Receive data from the server
+        if (recv(sock, buffer, MAX_BUFFER_SIZE, 0) <= 0) {
+            perror("Receive failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Server: %s", buffer);
+    }
+
+    close(sock);
+    return 0;
+}
