1790b3eec5e5356ce0dd974eb0ea7e0d80d1f9fc
Batman9818 <150887683+Batman9818@users.noreply.github.com>
2023-11-16 23:54:40 +0530

    Update exp3_server.c

--- exp3_server.c
+++ exp3_server.c
@@ -1,41 +1,68 @@
-#include<sys/types.h> 
-#include<sys/socket.h> 
-#include<netinet/in.h> 
-#include<netdb.h> 
-#include<arpa/inet.h> 
-#include<string.h> 
-#include<string.h> 
-#include<stdio.h> 
-int main(int asrgc,char*argv[]) 
-{ 
-int bd,sd,ad; 
-char buff[1024]; 
-struct sockaddr_in cliaddr,servaddr; 
-socklen_t clilen; 
-clilen=sizeof(cliaddr); 
-bzero(&servaddr,sizeof(servaddr)); 
- /*Socket address structure*/ 
-servaddr.sin_family=AF_INET; 
-servaddr.sin_addr.s_addr=htonl(INADDR_ANY); 
-servaddr.sin_port=htons(1999); 
-/*TCP socket is created, an Internet socket address structure is filled with wildcard address & 
-server’s well known port*/ 
-sd=socket(AF_INET,SOCK_STREAM,0); 
- /*Bind function assigns a local protocol address to the socket*/ 
-bd=bind(sd,(struct sockaddr*)&servaddr,sizeof(servaddr)); 
-/*Listen function specifies the maximum number of connections that kernel should queue for 
-this socket*/ 
-listen(sd,5); 
-printf("Server is running….\n"); 
- 
- /*The server to return the next completed connection from the front of the 
- completed connection Queue calls it*/ 
-ad=accept(sd,(struct sockaddr*)&cliaddr,&clilen); 
-while(1) 
-{ 
-bzero(&buff,sizeof(buff)); 
- /*Receiving the request message from the client*/ 
- recv(ad,buff,sizeof(buff),0); 
- printf("Message received is %s\n",buff); 
-} 
+#include<sys/socket.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<sys/types.h>
+#define PORT 2345
+#define MAX_BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[MAX_BUFFER_SIZE];
+
+    // Create socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set up sockaddr_in structure
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Bind the socket to localhost:8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    // Accept a connection
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("Accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+    	bzero(buffer,sizeof(buffer));
+        // Receive data from the client
+        if (recv(new_socket, buffer, MAX_BUFFER_SIZE, 0) <= 0) {
+            perror("Receive failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Client: %s", buffer);
+	bzero(buffer,sizeof(buffer));
+        // Send data to the client
+        printf("Server: ");
+        fgets(buffer, MAX_BUFFER_SIZE, stdin);
+        send(new_socket, buffer, strlen(buffer), 0);
+    }
+
+    close(server_fd);
+    return 0;
 }
