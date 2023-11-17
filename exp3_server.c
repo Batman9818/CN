f96766f68c021b90ec1d92e84ade55b86a023ed1
Batman9818 <150887683+Batman9818@users.noreply.github.com>
2023-11-17 18:45:01 +0530

    Update exp3_server.c

--- exp3_server.c
+++ exp3_server.c
@@ -9,60 +9,28 @@
 #include<sys/types.h>
 #define PORT 2345
 #define MAX_BUFFER_SIZE 1024
-
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     char buffer[MAX_BUFFER_SIZE];
-
-    // Create socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Set up sockaddr_in structure
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
-
-    // Bind the socket to localhost:8080
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Listen for incoming connections
-    if (listen(server_fd, 3) < 0) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
-    }
-
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
     printf("Server listening on port %d\n", PORT);
-
-    // Accept a connection
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("Accept failed");
-        exit(EXIT_FAILURE);
-    }
-
-    while (1) {
+    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+    while (1){
     	bzero(buffer,sizeof(buffer));
-        // Receive data from the client
-        if (recv(new_socket, buffer, MAX_BUFFER_SIZE, 0) <= 0) {
-            perror("Receive failed");
-            exit(EXIT_FAILURE);
-        }
-
+        recv(new_socket, buffer, MAX_BUFFER_SIZE, 0);
         printf("Client: %s", buffer);
 	bzero(buffer,sizeof(buffer));
-        // Send data to the client
         printf("Server: ");
         fgets(buffer, MAX_BUFFER_SIZE, stdin);
         send(new_socket, buffer, strlen(buffer), 0);
     }
-
     close(server_fd);
     return 0;
 }
