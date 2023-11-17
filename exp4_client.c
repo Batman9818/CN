637b9d2adb3df0a10361f1abe310023e80bb41d3
Batman9818 <150887683+Batman9818@users.noreply.github.com>
2023-11-17 18:46:10 +0530

    Update exp4_client.c

--- exp4_client.c
+++ exp4_client.c
@@ -14,38 +14,17 @@ int main() {
     int client_fd;
     struct sockaddr_in server_addr;
     char buffer[MAX_BUFFER_SIZE];
-
-    // Create socket file descriptor
-    if ((client_fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-        perror("Socket creation error");
-        exit(EXIT_FAILURE);
-    }
-
-    // Set up sockaddr_in structure for server
+    client_fd = socket(AF_INET, SOCK_DGRAM, 0);
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
-        perror("Invalid address/ Address not supported");
-        exit(EXIT_FAILURE);
-    }
-
     while (1) {
-        // Send data to the server
         printf("Client: ");
         fgets(buffer, MAX_BUFFER_SIZE, stdin);
         sendto(client_fd, buffer, strlen(buffer), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
 	bzero(buffer,sizeof(buffer));
-        // Receive data from the server
-        if (recvfrom(client_fd, buffer, MAX_BUFFER_SIZE, 0, NULL, NULL) == -1) {
-            perror("Receive failed");
-            exit(EXIT_FAILURE);
-        }
-
+        recvfrom(client_fd, buffer, MAX_BUFFER_SIZE, 0, NULL, NULL);
         printf("Server: %s", buffer);
     }
-
     close(client_fd);
     return 0;
 }
