001fe6469d8ded574ae6d3027920ac7a04e03a23
Batman9818 <150887683+Batman9818@users.noreply.github.com>
2023-11-17 18:45:23 +0530

    Update exp3_client.c

--- exp3_client.c
+++ exp3_client.c
@@ -14,38 +14,18 @@ int main() {
     int sock = 0;
     struct sockaddr_in serv_addr;
     char buffer[MAX_BUFFER_SIZE];
-
-    // Create socket file descriptor
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation error");
-        exit(EXIT_FAILURE);
-    }
-
-    // Set up sockaddr_in structure
+    sock = socket(AF_INET, SOCK_STREAM, 0) ;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-    
-    // Connect to the server
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Connection Failed");
-        exit(EXIT_FAILURE);
-    }
-
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     while (1) {
-        // Send data to the server
-        printf("Client: ");
+     	printf("Client: ");
         fgets(buffer, MAX_BUFFER_SIZE, stdin);
         send(sock, buffer, strlen(buffer), 0);
 	bzero(buffer,sizeof(buffer));
-        // Receive data from the server
-        if (recv(sock, buffer, MAX_BUFFER_SIZE, 0) <= 0) {
-            perror("Receive failed");
-            exit(EXIT_FAILURE);
-        }
-
+        recv(sock, buffer, MAX_BUFFER_SIZE, 0);
         printf("Server: %s", buffer);
     }
-
     close(sock);
     return 0;
 }
