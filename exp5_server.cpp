148355af0a77c149754b8d07ed93dc8ca5fa30e9
Batman9818 <150887683+Batman9818@users.noreply.github.com>
2023-11-17 18:46:42 +0530

    Update exp5_server.cpp

--- exp5_server.cpp
+++ exp5_server.cpp
@@ -6,7 +6,6 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-
 int main()
 {
     struct sockaddr_in sa;
@@ -16,41 +15,21 @@ int main()
     int ch = 0;
     char str[100];
     time_t tick;
-
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
-        printf("error in socket\n");
-        exit(0);
-    }
-    else
-        printf("Socket opened\n");
-
     memset(&sa, 0, sizeof(sa));
     sa.sin_port = htons(5600);
     sa.sin_addr.s_addr = htonl(0);
-
-    if (bind(sockfd, (struct sockaddr *)&sa, sizeof(sa)) < 0)
-    {
-        printf("Error in binding\n");
-    }
-    else
-        printf("Binded Successfully\n");
-
+    bind(sockfd, (struct sockaddr *)&sa, sizeof(sa));
     listen(sockfd, 50);
-
     for (;;)
     {
         len = sizeof(ch);
         conntfd = accept(sockfd, (struct sockaddr *)&cli, &len);
         printf("Accepted");
-
         tick = time(NULL);
         snprintf(str, sizeof(str), "%s", ctime(&tick));
-        printf("%s", str);
-
+        printf("\n%s", str);
         write(conntfd, str, strlen(str));
     }
-    
     return 0;
 }
