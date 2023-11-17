102004213229ec442f01e6b3f48b56ef84742596
Batman9818 <150887683+Batman9818@users.noreply.github.com>
2023-11-17 18:45:50 +0530

    Update exp4_server.c

--- exp4_server.c
+++ exp4_server.c
@@ -15,42 +15,21 @@ int main() {
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_len = sizeof(client_addr);
     char buffer[MAX_BUFFER_SIZE];
-
-    // Create socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Set up sockaddr_in structure for server
+    server_fd = socket(AF_INET, SOCK_DGRAM, 0);
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
-
-    // Bind the socket to localhost:8080
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
-    }
-
+    bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     printf("Server listening on port %d\n", PORT);
-
     while (1) {
     	bzero(buffer,sizeof(buffer));
-        // Receive data from the client
-        if (recvfrom(server_fd, buffer, MAX_BUFFER_SIZE, 0, (struct sockaddr*)&client_addr, &addr_len) == -1) {
-            perror("Receive failed");
-            exit(EXIT_FAILURE);
-        }
-
+        recvfrom(server_fd, buffer, MAX_BUFFER_SIZE, 0, (struct sockaddr*)&client_addr, &addr_len);
         printf("Client: %s", buffer);
 	bzero(buffer,sizeof(buffer));
-        // Send data to the client
         printf("Server: ");
         fgets(buffer, MAX_BUFFER_SIZE, stdin);
         sendto(server_fd, buffer, strlen(buffer), 0, (struct sockaddr*)&client_addr, addr_len);
     }
-
     close(server_fd);
     return 0;
 }
